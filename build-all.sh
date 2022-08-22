c913c5c2e8d33811d3ab73c2aecfd08abf0224f6
Troels Liebe Bentsen <tlb@connectedcars.io>
2022-08-23 00:13:19 +0200

    Fix external variable names

--- build-all.sh
+++ build-all.sh
@@ -6,29 +6,28 @@ NODE_VERSIONS="18.7.0"
 # 16.16.0 14.20.0 12.22.12"
 YARN_VERSION="1.22.19"
 NPM_VERSION="8.16.0"
-BUILD_PLATFORMS='linux/amd64 linux/arm64'
+BUILD_PLATFORMS='linux/arm64'
 
-BUILD_PROJECT_ID=${PROJECT_ID:-}
-BUILD_NPM_TOKEN=${NPM_TOKEN:-}
-BUILD_BRANCH_NAME=${BRANCH_NAME:-}
-BUILD_PUSH=${PUSH:-}
+# External variables
+PROJECT_ID=${PROJECT_ID:-}
+NPM_TOKEN=${NPM_TOKEN:-}
+BRANCH_NAME=${BRANCH_NAME:-}
+PUSH=${PUSH:-}
 
-if [[ ! -n "$BUILD_PROJECT_ID" ]]; then
+if [[ ! -n "$PROJECT_ID" ]]; then
     echo "PROJECT_ID needs to be set"
     exit 255
 fi
 
-if [[ ! -n "$BUILD_NPM_TOKEN" ]]; then
+if [[ ! -n "$NPM_TOKEN" ]]; then
     echo "NPM_TOKEN needs to be set"
     exit 255
 fi
 
-if [[ ! -n "$BUILD_BRANCH_NAME" ]]; then
+if [[ ! -n "$BRANCH_NAME" ]]; then
     echo "BRANCH_NAME needs to be set"
     exit 255
 fi
-
-echo ${BUILD_PROJECT_ID}
 
 DOCKER_PLATFORMS=$(echo $BUILD_PLATFORMS | sed "s/ /,/g")
 
@@ -38,7 +37,7 @@ for NODE_VERSION in $NODE_VERSIONS; do
     NODE_MAJOR_VERSION=$(echo $NODE_VERSION | cut -d. -f1)
     
     DOCKER_NODE_BUILD_ARGS="--build-arg=NODE_VERSION=${NODE_VERSION} --build-arg=NPM_VERSION=${NPM_VERSION} --build-arg=YARN_VERSION=${YARN_VERSION}"
-    DOCKER_TEST_BUILD_ARGS="--build-arg=NODE_VERSION=${NODE_VERSION} --build-arg=NPM_TOKEN=${BUILD_NPM_TOKEN} --build-arg=BRANCH_NAME=${BUILD_BRANCH_NAME}"
+    DOCKER_TEST_BUILD_ARGS="--build-arg=NODE_VERSION=${NODE_VERSION} --build-arg=NPM_TOKEN=${NPM_TOKEN} --build-arg=BRANCH_NAME=${BRANCH_NAME}"
 
     # Build image cache for all platforms so it's ready
     echo "Building node $NODE_VERSION images for $BUILD_PLATFORMS";
@@ -47,33 +46,34 @@ for NODE_VERSION in $NODE_VERSIONS; do
     # Build test images to see it work
     for PLATFORM in $BUILD_PLATFORMS; do
         echo "Load node $NODE_VERSION builder and base image into dockers image store for $PLATFORM"
-        docker buildx build --platform=${PLATFORM} --progress=plain --target=base --load --tag=gcr.io/${PROJECT_ID}/node-base.${BUILD_BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
-        docker buildx build --platform=${PLATFORM} --progress=plain --target=builder --load --tag=gcr.io/${PROJECT_ID}/node-builder.${BUILD_BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
+        docker buildx build --platform=${PLATFORM} --progress=plain --target=base --load --tag=gcr.io/${PROJECT_ID}/node-base.${BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
+        docker buildx build --platform=${PLATFORM} --progress=plain --target=builder --load --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
 
         echo "Building test image for node $NODE_VERSION for $PLATFORM";
         docker buildx build --platform=${PLATFORM} --progress=plain ${DOCKER_TEST_BUILD_ARGS} --builder default --load --tag=test:${NODE_VERSION} test/
+        echo docker buildx build --platform=${PLATFORM} --progress=plain ${DOCKER_TEST_BUILD_ARGS} --target=builder --builder default --load --tag=test-builder:${NODE_VERSION} test/
         
         echo "Running test image for node $NODE_VERSION for $PLATFORM";
         docker run --platform=${PLATFORM} test:${NODE_VERSION}
     done
 
-    if [[ -n "$BUILD_PUSH" ]]; then
+    if [[ -n "$PUSH" ]]; then
         echo Push base images
         docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=base ${DOCKER_NODE_BUILD_ARGS} --push \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-base.${BUILD_BRANCH_NAME}:${NODE_VERSION} \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-base.${BUILD_BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
+        --tag=gcr.io/${PROJECT_ID}/node-base.${BRANCH_NAME}:${NODE_VERSION} \
+        --tag=gcr.io/${PROJECT_ID}/node-base.${BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
         .
 
         echo Push builder images
         docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=base ${DOCKER_NODE_BUILD_ARGS} --push \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-builder.${BUILD_BRANCH_NAME}:$NODE_VERSION.x \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-builder.${BUILD_BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
+        --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:$NODE_VERSION.x \
+        --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
         .
 
         echo Push fat-base images
         docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=base ${DOCKER_NODE_BUILD_ARGS} --push \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-fat-base.${BUILD_BRANCH_NAME}:${NODE_VERSION} \
-        --tag=gcr.io/${BUILD_PROJECT_ID}/node-fat-base.${BUILD_BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
+        --tag=gcr.io/${PROJECT_ID}/node-fat-base.${BRANCH_NAME}:${NODE_VERSION} \
+        --tag=gcr.io/${PROJECT_ID}/node-fat-base.${BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
         .  
     fi
 done
