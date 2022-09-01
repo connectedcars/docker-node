4b785b484b9ca2250323f8c2f39a10bc652f45fb
Troels Liebe Bentsen <troels@connectedcars.dk>
2022-09-01 10:10:50 +0200

    Fix target paths when pushing

--- build-all.sh
+++ build-all.sh
@@ -47,6 +47,7 @@ for NODE_VERSION in $NODE_VERSIONS; do
         echo "Load node $NODE_VERSION builder and base image into dockers image store for $PLATFORM"
         docker buildx build --platform=${PLATFORM} --progress=plain --target=base --load --tag=gcr.io/${PROJECT_ID}/node-base.${BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
         docker buildx build --platform=${PLATFORM} --progress=plain --target=builder --load --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
+        docker buildx build --platform=${PLATFORM} --progress=plain --target=fat-base --load --tag=gcr.io/${PROJECT_ID}/node-fat-base.${BRANCH_NAME}:${NODE_VERSION} ${DOCKER_NODE_BUILD_ARGS} .
 
         echo "Building test image for node $NODE_VERSION for $PLATFORM";
         docker buildx build --platform=${PLATFORM} --progress=plain ${DOCKER_TEST_BUILD_ARGS} --builder default --load --tag=test:${NODE_VERSION} test/
@@ -64,13 +65,13 @@ for NODE_VERSION in $NODE_VERSIONS; do
         .
 
         echo Push builder images
-        docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=base ${DOCKER_NODE_BUILD_ARGS} --push \
+        docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=builder ${DOCKER_NODE_BUILD_ARGS} --push \
         --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:$NODE_VERSION.x \
         --tag=gcr.io/${PROJECT_ID}/node-builder.${BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
         .
 
         echo Push fat-base images
-        docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=base ${DOCKER_NODE_BUILD_ARGS} --push \
+        docker buildx build --platform=${DOCKER_PLATFORMS} --progress=plain --target=fat-base ${DOCKER_NODE_BUILD_ARGS} --push \
         --tag=gcr.io/${PROJECT_ID}/node-fat-base.${BRANCH_NAME}:${NODE_VERSION} \
         --tag=gcr.io/${PROJECT_ID}/node-fat-base.${BRANCH_NAME}:$NODE_MAJOR_VERSION.x \
         .  
